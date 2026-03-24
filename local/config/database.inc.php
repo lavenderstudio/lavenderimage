1ce3133bfc9097bda6930df20f6ce9ccb0cbd44c
lavenderstudio <craftstudiovietnam@gmail.com>
2026-03-24 17:10:18 +0700

    Update database.inc.php

--- local/config/database.inc.php
+++ local/config/database.inc.php
@@ -1,10 +1,9 @@
 <?php
-$conf['db_host'] = getenv('MYSQLHOST') . ':' . getenv('MYSQLPORT');
-$conf['db_user'] = getenv('MYSQLUSER');
-$conf['db_password'] = getenv('MYSQLPASSWORD');
-$conf['db_base'] = getenv('MYSQLDATABASE');
+$conf['db_host'] = 'mysql.railway.internal:3306'; 
+$conf['db_user'] = 'root';
+$conf['db_password'] = 'yEaKItfAreoFBaWShRQAhOvZaBZiqgvW';
+$conf['db_base'] = 'railway';
 $conf['db_prefix'] = 'piwigo_';
 
-// DÒNG QUYẾT ĐỊNH: Báo cho Piwigo biết hệ thống đã sẵn sàng
 define('PHPWG_INSTALLED', true);
 ?>
