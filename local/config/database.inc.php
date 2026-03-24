93a1985238a69d1a6e8383b23a1096d9b8f92da5
lavenderstudio <craftstudiovietnam@gmail.com>
2026-03-24 19:22:14 +0700

    Delete local/config/database.inc.php

--- local/config/database.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conf['db_host'] = 'mysql.railway.internal:3306'; 
-$conf['db_user'] = 'root';
-$conf['db_password'] = 'yEaKItfAreoFBaWShRQAhOvZaBZiqgvW';
-$conf['db_base'] = 'railway';
-$conf['db_prefix'] = 'piwigo_';
-
-define('PHPWG_INSTALLED', true);
-?>
