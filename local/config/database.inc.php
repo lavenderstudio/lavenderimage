46c1dcfc2aa018c707d69b86448811f9ce622e40
lavenderstudio <craftstudiovietnam@gmail.com>
2026-03-24 16:57:48 +0700

    Update database.inc.php

--- local/config/database.inc.php
+++ local/config/database.inc.php
@@ -4,5 +4,7 @@ $conf['db_user'] = getenv('MYSQLUSER');
 $conf['db_password'] = getenv('MYSQLPASSWORD');
 $conf['db_base'] = getenv('MYSQLDATABASE');
 $conf['db_prefix'] = 'piwigo_';
-$conf['password_hash'] = 'bcrypt';
+
+// DÒNG QUAN TRỌNG NHẤT: Báo hiệu đã cài đặt xong
+define('PHPWG_INSTALLED', true);
 ?>
